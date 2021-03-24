da000aaa32c48f4b2892a1c6273fbc6da7d5709c
nhsz <nh.quiroz@gmail.com>
2021-03-24 16:31:51 -0300

    test

---
+++ .eslintrc.js
@@ -0,0 +1,63 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es6: true
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2020
+  },
+  ignorePatterns: ['node_modules/*', '.next/*', '.out/*', '!.prettierrc.js'], // We don't want to lint generated files nor node_modules, but we want to lint .prettierrc.js (ignored by default by eslint)
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  overrides: [
+    // This configuration will apply only to TypeScript files
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 2020
+      },
+      settings: { react: { version: 'detect' } },
+      env: {
+        browser: true,
+        node: true,
+        es6: true
+      },
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:prettier/recommended',
+        'plugin:jsx-a11y/recommended'
+      ],
+      rules: {
+        'react/prop-types': 'off',
+        // No need to import React when using Next.js
+        'react/react-in-jsx-scope': 'off',
+        // This rule is not compatible with Next.js's <Link /> components
+        'jsx-a11y/anchor-is-valid': 'off',
+        '@typescript-eslint/no-unused-vars': ['error'],
+        // I suggest this setting for requiring return types on functions only where useful
+        '@typescript-eslint/explicit-function-return-type': [
+          'warn',
+          {
+            allowExpressions: true,
+            allowConciseArrowFunctionExpressionsStartingWithVoid: true
+          }
+        ],
+        'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+        complexity: [2, 3],
+        'max-statements': [2, 7],
+        'max-statements-per-line': [2, { max: 1 }],
+        'max-nested-callbacks': [2, 2],
+        'max-depth': [2, { max: 2 }],
+        'prefer-const': 2
+      }
+    }
+  ],
+  settings: { react: { version: 'detect' } }
+};
---
+++ pages/index.tsx
@@ -0,0 +1,62 @@
+// import Head from 'next/head';
+
+// export default function Home() {
+//   return (
+//     <div>
+//       <Head>
+//         <title>Create Next App</title>
+//         <link rel='icon' href='/favicon.ico' />
+//       </Head>
+
+//       <main>
+//         <h1>
+//           Welcome to <a href='https://nextjs.org'>Next.js!</a>
+//         </h1>
+//       </main>
+
+//       <footer></footer>
+//     </div>
+//   );
+// }
+
+import { GetServerSideProps, NextPage } from 'next';
+
+interface Props {
+  launch: {
+    mission: string;
+    site: string;
+    timestamp: number;
+    rocket: string;
+  };
+}
+
+{
+}
+
+const IndexPage: NextPage<Props> = ({ launch }) => {
+  const date = new Date(launch.timestamp);
+  return (
+    <main>
+      <h1>Next SpaceX Launch: {launch.mission}</h1>
+      <p>
+        {launch.rocket} will take off from {launch.site} on {date.toDateString()}
+      </p>
+    </main>
+  );
+};
+export default IndexPage;
+
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  const response = await fetch('https://api.spacexdata.com/v3/launches/next');
+  const nextLaunch = await response.json();
+  return {
+    props: {
+      launch: {
+        mission: nextLaunch.mission_name,
+        site: nextLaunch.launch_site.site_name_long,
+        timestamp: nextLaunch.launch_date_unix * 1000,
+        rocket: nextLaunch.rocket.rocket_name
+      }
+    }
+  };
+};
