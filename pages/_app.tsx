9cc4e164c785e74b26ae41fa428aea305eea15a9
nhsz <nh.quiroz@gmail.com>
2021-03-26 13:40:52 -0300

    analytics: add splitbee

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,13 @@
+import splitbee from '@splitbee/web';
 import { ThemeProvider } from 'next-themes';
 import { AppProps } from 'next/app';
+import { useEffect } from 'react';
 import { Layout } from '../components';
 import '../styles/globals.css';
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
+  useEffect(() => splitbee.init(), []);
+
   return (
     <ThemeProvider attribute='class'>
       <Layout>
